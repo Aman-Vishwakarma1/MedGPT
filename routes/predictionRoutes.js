9799f132d1cf28475d98f44b01d5398674e8ba88
Aman Vishwakarma <me.amanvishwakarma@gmail.com>
2025-04-22 00:59:59 +0530

    multer issuse in vercel.

--- routes/predictionRoutes.js
+++ routes/predictionRoutes.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const multer = require("multer");
 const predict = require("../controllers/predictionController");
 
-const upload = multer({ dest: "uploads/" });
+// const upload = multer({ dest: "uploads/" });
+const upload = multer({ storage: multer.memoryStorage() });
 
 router.post("/predict", upload.single("image"), predict.predict);
 router.post("/chat", predict.chat);
